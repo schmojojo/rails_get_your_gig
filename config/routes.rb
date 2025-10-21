aaee3253649e8148133c05b761f9226a6dab1e0d
Johannes Hofmann <johannes.siegfried.hofmann@googlemail.com>
2025-10-21 12:50:57 +0200

    finished route controller#action view for list all events and see detail of events

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/show'
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#events"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :events, only: [:index, :show]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
