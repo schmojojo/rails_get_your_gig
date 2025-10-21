608963863c8e69dd0b77f804feccecad63fc093f
Dumitru Baltatescu <dumitrubaltatescu.work@gmail.com>
2025-10-21 17:15:09 +0200

    fixed routing issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'events/index'
-  get 'events/show'
   devise_for :users
-  root to: "pages#events"
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :events, only: [:index, :show, :new, :create]
