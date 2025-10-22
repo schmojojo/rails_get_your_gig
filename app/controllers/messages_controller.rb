f566e33b9b3f570faac3e053db891eec855830bc
Dumitru Baltatescu <dumitrubaltatescu.work@gmail.com>
2025-10-22 17:43:04 +0200

    Implemented full AI assistant chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,36 +1,57 @@
 class MessagesController < ApplicationController
-  SYSTEM_PROMPT = "You are an expert file clerk.\n\nI am a disorganized person, looking at an event calendar.\n\nCategorize the event list according to the keywords i give you.\n\nAnswer consicely in Markdown."
+  before_action :authenticate_user!
+  SYSTEM_PROMPT = "You are an expert file clerk helping a disorganized person categorize events from a calendar. Use the user's keywords to create categories and list the relevant events under each. Answer concisely in Markdown."
 
-  def new
-    @events = Event.all
+  def index
+    @messages = current_user.messages.order(created_at: :asc)
     @message = Message.new
-    @messages = Message.all
   end
 
   def create
-    @events = Event.all
-    @messages = Message.all
-    @message = Message.new(role: "User", content: params[:message][:content])
-    if @message.save
-      @ruby_llm_chat = RubyLLM.chat
-      response = @ruby_llm_chat.with_instructions(instructions).ask(@message.content)
-      Message.create(role: "Assistant", content: response.content)
-      redirect_to new_message_path
-    else
-      render :new, status: :unprocessable_entity
-    end
-
+    user_message = current_user.messages.create(message_params.merge(role: "user"))
+    ai_client = RubyLLM.chat
+    response = ai_client.with_instructions(instructions).ask(user_message.content)
+    current_user.messages.create(role: "assistant", content: response.content)
+    redirect_to messages_path
   end
 
-  def events_list
-    text = "here's the info about the events in the db:"
-    @events.each do |event|
-      text += " event title: #{event.title}, location: #{event.location}, event date: #{event.date}, event category: #{event.category}, event description: #{event.description}, event contact: #{event.contact}"
+  # def new
+  #   @events = Event.all
+  #   @message = Message.new
+  #   @messages = Message.all
+  # end
+
+  # def create
+  #   @events = Event.all
+  #   @messages = Message.all
+  #   @message = Message.new(role: "User", content: params[:message][:content])
+  #   if @message.save
+  #     @ruby_llm_chat = RubyLLM.chat
+  #     response = @ruby_llm_chat.with_instructions(instructions).ask(@message.content)
+  #     Message.create(role: "Assistant", content: response.content)
+  #     redirect_to new_message_path
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+
+  private
+
+ def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def event_context
+    events = Event.all.order(date: :asc)
+
+    event_details = events.map do |event|
+      "- Title: #{event.title}, Date: #{event.date}, Location: #{event.location}, Category: #{event.category}"
     end
+
+    "Here is the current list of available events:\n#{event_details.join("\n")}"
   end
 
   def instructions
-    [SYSTEM_PROMPT, events_list].compact.join("\n\n")
+    [SYSTEM_PROMPT, event_context].join("\n\n")
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_22_124947) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_22_145116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_22_124947) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +49,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_22_124947) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "messages", "users"
 end
