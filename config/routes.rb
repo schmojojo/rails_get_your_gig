6a2850ca6237da12d991aad412068e71a4b9f6dc
Jan Calderon <jancal5@outlook.com>
2025-10-22 15:25:22 +0200

    added message MCV, initial ai integration

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,9 @@ Rails.application.routes.draw do
 
   resources :events, only: [:index, :show, :new, :create]
 
+  resources :messages
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/pages/home", to: "pages#home"
-
-  resources :chats, only: [:create, :show] do
-    resources :messages #, only: [:create]
-  end
 
 end
