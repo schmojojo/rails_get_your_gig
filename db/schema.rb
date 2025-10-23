f24245d4ecc42ebbcb5d40ff2ef920696a9ea66e
Johannes Hofmann <johannes.siegfried.hofmann@googlemail.com>
2025-10-23 17:07:21 +0200

    global styling saved

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_23_135409) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,4 +79,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_23_135409) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "events", "users"
+  add_foreign_key "messages", "users"
 end
